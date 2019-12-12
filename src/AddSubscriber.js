4ae656e1675eb640d80a08b0418ef65737f0f382
shreya <shreya171769.cse@chitkara.in>
2019-12-12 15:36:08 +0530

    State: Added state in ‘AddSubscriber’ and ‘App’ components

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -3,54 +3,40 @@ import Header from "./Header";
 import "./AddSubscriber.css";
 
 class AddSusbscriber extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
+    };
+  }
+
+  inputChangedHandler = e => {
+    const state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  };
+
   render() {
     return (
       <div>
-        <Header heading="Add Subscriber" />
-        <div className="component-body-container">
-          <button className="custom-btn">Back</button>
+        <div>
+          <Header heading="Phone Directory" />
 
-          <form className="subscriber-form">
-            <label htmlFor="name" className="label-control">
-              Name:{" "}
-            </label>
-            <br />
-            <input
-              id="name"
-              type="text"
-              className="input-control"
-              name="name"
-            />
-            <br />
-            <br />
-            <label htmlFor="phone" className="label-control">
-              Phone:{" "}
-            </label>
-            <br />
-            <input
-              id="phone"
-              type="text"
-              className="input-control"
-              name="phone"
-            />
-            <br />
-            <br />
-
-            <div className="subscriber-info-container">
-              <span className="subscriber-to-add-heading">
-                Subscriber to be added:{" "}
-              </span>
-              <br />
-              <span className="subscriber-info">Name: </span>
-              <br />
-              <span className="subscriber-info">Phone: </span>
-              <br />
-            </div>
-
-            <button type="submit" className="custom-btn add-btn">
-              Add
-            </button>
-          </form>
+          <span className="grid-item name-heading">Name</span>
+          <span className="grid-item phone-heading">Phone</span>
+        </div>
+        <div>
+          {this.state.subscribersListToShow.map(sub => {
+            return (
+              <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
--- src/App.js
+++ src/App.js
@@ -3,40 +3,61 @@ import Header from "./Header.js";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    let subscribers = [
-      {
-        id: 1,
-        name: "Shilpa Bhat",
-        phone: "8888888888"
-      },
-      {
-        id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
-      }
-    ];
+  constructor() {
+    super();
+    this.state = {
+      subscribersListToShow: []
+    };
+  }
 
+  render() {
     return (
       <div>
-        <div>
-          <Header heading="Phone Directory" />
+        <Header heading="Add Subscriber" />
+        <div className="component-body-container">
+          <button className="custom-btn">Back</button>
 
-          <span className="grid-item name-heading">Name</span>
-          <span className="grid-item phone-heading">Phone</span>
-        </div>
-        <div>
-          {subscribers.map(sub => {
-            return (
-              <div key={sub.id} className="grid-container">
-                <span className="grid-item">{sub.name}</span>
-                <span className="grid-item">{sub.phone}</span>
-                <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
-                </span>
-              </div>
-            );
-          })}
+          <form className="subscriber-form">
+            <label htmlFor="name" className="label-control">
+              Name:{" "}
+            </label>
+            <br />
+            <input
+              id="name"
+              type="text"
+              className="input-control"
+              name="name"
+            />
+            <br />
+            <br />
+            <label htmlFor="phone" className="label-control">
+              Phone:{" "}
+            </label>
+            <br />
+            <input
+              id="phone"
+              type="text"
+              className="input-control"
+              name="phone"
+            />
+            <br />
+            <br />
+
+            <div className="subscriber-info-container">
+              <span className="subscriber-to-add-heading">
+                Subscriber to be added:{" "}
+              </span>
+              <br />
+              <span className="subscriber-info">Name: </span>
+              <br />
+              <span className="subscriber-info">Phone: </span>
+              <br />
+            </div>
+
+            <button type="submit" className="custom-btn add-btn">
+              Add
+            </button>
+          </form>
         </div>
       </div>
     );
